77eab04ac87d129ce53e2b46d4449521c0552a4a
tavis <tavis@tav.is>
2014-04-27 22:48:03 -0400

    Add shareworks scraper

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 
 
 var express = require('express');
+var shareworks = require('./lib/shareworks.js');
 var scraper = require('./lib/scrape.js');
 var app = express();
 
@@ -16,23 +17,9 @@ app.get('/', function(req, res){
 });
 
 app.get('/scrape', function(req, res){
-    go(req,res);
+    shareworks.do_request(function(val){
+        res.send(val);
+    })
 });
-
-
-go = function(req,res) {
-    var stuff = '';
-
-    stuff = scraper.test;
-    res.send(stuff);
-}
-
-foo = function(req, res){
-    $ = cheerio.load('<html><head></head><body><div id="content"><div id="sidebar"></div><div id="main"><div id="breadcrumbs"></div><table id="data"><tr><th>Name</th><th>Address</th></tr><tr><td class="name">John</td><td class="address">Address of John</td></tr><tr><td class="name">Susan</td><td class="address">Address of Susan</td></tr></table></div></div></body></html>');
-    $('#data .name').each(function() {
-	res.send($(this).text());
-    });
-
-}
 
 app.listen(3001);
